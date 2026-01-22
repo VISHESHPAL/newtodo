977a9501ac820ece810c8579c03de9330905393e
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2026-01-22 18:23:11 +0530

    adding the cors property

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://newtodo-lxvl.vercel.app"],
+    origin: ["https://newtodo-lxvl.vercel.app","http://localhost:5173"],
     credentials: true,
   }),
 );
